6e6ee26f4eb26811e4f115767b0374e7a9a91415
Zuleika Tesei <zuleikatesei@zuleikas-mbp.lan>
2022-08-23 11:31:15 -0700

    uvi index colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 // Define Variables 
 var apiKey = "fce6bd9d569c80c55e49fc3d9cabf2fe"
 var baseUrl = "http://api.openweathermap.org"
+var today = moment().format('L');
 
 var searchInput = document.querySelector("#search-input");
 var searchForm = document.querySelector("#search-form");
-var displayWeatherEl = document.querySelector("#city-weather");
 var cityName = document.querySelector("#city-name");
 var cityTemp = document.querySelector("#city-temp");
 var cityWind = document.querySelector("#city-wind");
@@ -20,9 +20,13 @@ function fetchCoords(search) {
     .then((response) => response.json())
     .then((data) => { console.log(data) 
         fetchWeather(data.coord)
-        cityName.textContent = data.name;
-    });
+        
+        var cityName = $(`
+            <h2 id="city-name"> ${data.name} ${today} </h2> 
+        `)
 
+        $("#city-name").append(cityName);
+    });
 }
 
 function handleSearchForm(e) {
@@ -45,7 +49,29 @@ function fetchWeather(coord) {
         cityTemp.innerHTML = `Temp: ${data.current.temp} °F`;
         cityWind.innerHTML = `Wind: ${data.current.wind_speed} MPH`;
         cityHumid.innerHTML = `Humidity: ${data.current.humidity} %`;
-        cityUv.innerHTML = `UV Index: ${data.current.uvi}`;
+        // cityUv.innerHTML = `UV Index: ${data.current.uvi}`;
+
+        // uvi index 
+        var uvIndex = data.current.uvi;
+        var uvIndexInfo = $(`
+            <p> UV Index: 
+                <span id="uvIndexColor" class="px-2 py-2 rounded"> ${uvIndex} </span>
+            </p> 
+        `);
+
+        $("#city-weather").append(uvIndexInfo)
+
+        if (uvIndex >= 0 && uvIndex <= 2) {
+            $("#uvIndexColor").css("background-color", "#3EA72D").css("color", "white");
+        } else if (uvIndex >= 3 && uvIndex <= 5) {
+            $("#uvIndexColor").css("background-color", "#FFF300");
+        } else if (uvIndex >= 6 && uvIndex <= 7) {
+            $("#uvIndexColor").css("background-color", "#F18B00");
+        } else if (uvIndex >= 8 && uvIndex <= 10) {
+            $("#uvIndexColor").css("background-color", "#E53210").css("color", "white");
+        } else {
+            $("#uvIndexColor").css("background-color", "#B567A4").css("color", "white"); 
+        }; 
 
         // render five day forecast 
         for (let i = 1; i < 6; i++) {
@@ -57,12 +83,14 @@ function fetchWeather(coord) {
             };
             
             var currentDate = moment.unix(cityInfo.date).format("MM/DD/YYYY");
+            var iconURL = `<img src="https://openweathermap.org/img/w/${cityInfo.icon}.png"`
     
             var futureCard = $(`
             <div class="pl-3">
                 <div class="card pl-3 pt-3 mb-3 bg-primary text-light">
                     <div class="card-body"> 
                         <h5> ${currentDate}</h5> 
+                        <p> ${iconURL} </p>
                         <p> Temp: ${cityInfo.temp} °F</p>
                         <p> Humidity: ${cityInfo.humidity}\%</p>
                     </div> 
